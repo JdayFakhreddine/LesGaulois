4d932fac81a21cafaa190a75ee0edb5b119df3cf
JdayFakhreddine <114723243+JdayFakhreddine@users.noreply.github.com>
2022-10-07 09:35:12 +0200

    fin Tp1

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -4,20 +4,25 @@ public class Druide {
 	private String nom;
 	private int effetPotionMin;
 	private int effetPotionMax;
+
 	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
-	this.nom = nom;
-	this.effetPotionMin = effetPotionMin;
-	this.effetPotionMax = effetPotionMax;
-	parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
 	}
+
 	public String getNom() {
-	return nom;
+		return nom;
 	}
+
 	public void parler(String texte) {
-	System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+
 	private String prendreParole() {
-	return "Le druide " + nom + " : ";
+		return "Le druide " + nom + " : ";
 	}
 
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,30 +3,37 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
-	private int effetPotion =1;
+	private int effetPotion = 1;
+
 	public Gaulois(String nom, int force) {
-	this.nom = nom;
-	this.force = force;
+		this.nom = nom;
+		this.force = force;
 	}
+
 	public String getNom() {
-	return nom;
+		return nom;
 	}
+
 	public void parler(String texte) {
-	System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+
 	private String prendreParole() {
-	return "Le gaulois " + nom + " : ";
+		return "Le gaulois " + nom + " : ";
 	}
+
 	public void frapper(Romain romain) {
-	System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-	romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
 	}
+
 	@Override
 	public String toString() {
-	return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
+
 	public static void main(String[] args) {
-	//TODO créer un main permettant de tester la classe Gaulois
+		// TODO créer un main permettant de tester la classe Gaulois
 	}
 
 }
